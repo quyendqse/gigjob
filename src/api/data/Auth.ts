e3160ed7dfdc929a41fafc8ed8c613e19e0e63ae
Thuyen Vu <thuyenvuanh2412@gmail.com>
2023-03-12 11:42:06 +0700

    edit

--- src/api/data/Auth.ts
+++ src/api/data/Auth.ts
@@ -1,12 +1,15 @@
 import { UserCredential } from "@firebase/auth";
 import { logOut } from "../../firebase/firebase";
 async function loginGoogle(user: UserCredential) {
-  var res = await fetch("http://localhost:8082/api/v1/account/login/google", {
-    method: "POST",
-    headers: {
-      idTokenString: await user.user.getIdToken(),
-    },
-  });
+  var res = await fetch(
+    "http://54.179.205.85:8080/api/v1/account/login/google",
+    {
+      method: "POST",
+      headers: {
+        idTokenString: await user.user.getIdToken(),
+      },
+    }
+  );
   if (res.status === 200) {
     let data = await res.json();
     sessionStorage.setItem("accessToken", data.accessToken);
